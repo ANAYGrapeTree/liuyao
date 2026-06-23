40eea40a0e4adb6872abacf03dc0ff4b52e174b4
NengyuPersonal <nengyuluomty@gmail.com>
2026-06-24 00:28:49 +0800

    fix: suppress unused variable lint error in JieGua component

--- src/components/JieGua.tsx
+++ src/components/JieGua.tsx
@@ -7,6 +7,7 @@ interface JieGuaProps {
   onBack: () => void;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function JieGua({ paiguaResult, questionType, onBack }: JieGuaProps) {
   return (
     <div className="min-h-screen bg-gradient-to-br from-primary-50 via-white to-secondary-50">
